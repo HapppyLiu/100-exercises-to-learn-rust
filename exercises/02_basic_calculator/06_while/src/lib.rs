6b7a1c0b577f9c525cc43c167e999fb53ac576b8
andy <andy@andy.com>
2024-06-02 11:25:39 +0800

    solve chapter 2's exercises

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,17 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut result = 1;
+    let mut num = n;
+    if n == 0 {
+        1
+    } else {
+        while num > 1 {
+            result *= num;
+            num -= 1;
+        }
+        result
+    }
 }
 
 #[cfg(test)]
